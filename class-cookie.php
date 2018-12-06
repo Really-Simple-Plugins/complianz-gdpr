1660629e0ab0480704e444a79ef33e9e6b01c9e7
Rogier Lankhorst <rogier@really-simple-plugins.com>
2018-12-06 13:23:04 +0100

    youtube placeholder fix, tm merge

--- class-cookie.php
+++ class-cookie.php
@@ -452,7 +452,8 @@ if (!class_exists("cmplz_cookie")) {
 
                 $output['readmore_url'] = get_option('cmplz_url_cookie-statement');
                 $output['readmore_url_us'] = get_option('cmplz_url_cookie-statement-us');
-                $output['privacy_link'] = !empty(get_option('cmplz_url_privacy-statement-us')) ? '&nbsp;-&nbsp;<a aria-label="learn more about privacy" tabindex="0" class="cc-link" href="' . get_option('cmplz_url_privacy-statement-us') . '">' . $output['readmore_privacy'] . '</a>' : '';
+                $privacy_link = empty(get_option('cmplz_url_privacy-statement-us')) ? cmplz_get_value('custom_privacy_statement_url') : !empty(get_option('cmplz_url_privacy-statement-us'));
+                $output['privacy_link'] = !empty($privacy_link) ? '&nbsp;-&nbsp;<a aria-label="learn more about privacy" tabindex="0" class="cc-link" href="' . $privacy_link . '">' . $output['readmore_privacy'] . '</a>' : '';
 
                 $output['nonce'] = wp_create_nonce('set_cookie');
                 $output['url'] = admin_url('admin-ajax.php');
