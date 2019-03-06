5c455ae158da5eef2e753ca9b69924fb1c27e2e1
Rogier Lankhorst <rogier@really-simple-plugins.com>
2019-03-06 17:02:44 +0100

    version bump

--- complianz-gpdr.php
+++ complianz-gpdr.php
@@ -3,7 +3,7 @@
  * Plugin Name: Complianz | GDPR Cookie Consent
  * Plugin URI: https://www.wordpress.org/plugins/complianz-gdpr
  * Description: Complianz Privacy Suite for GDPR, CaCPA, DSVGO, AVG with a conditional cookie warning and customized cookie policy
- * Version: 2.1.7
+ * Version: 2.1.8
  * Text Domain: complianz-gdpr
  * Domain Path: /languages
  * Author: RogierLankhorst, Complianz team
