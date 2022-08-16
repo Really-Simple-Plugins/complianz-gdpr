1c2c7678192659e9ba89c98569727e38e07560dc
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-08-16 09:56:04 +0200

    text changes

--- complianz-gpdr.php
+++ complianz-gpdr.php
@@ -39,14 +39,14 @@ if ( ! function_exists( 'cmplz_activation_check' ) ) {
 	function cmplz_activation_check() {
 		if ( version_compare( PHP_VERSION, '7.2', '<' ) ) {
 			deactivate_plugins( plugin_basename( __FILE__ ) );
-			wp_die( __( 'Complianz GDPR cannot be activated. The plugin requires PHP 5.6 or higher',
+			wp_die( __( 'Complianz GDPR cannot be activated. The plugin requires PHP 7.2 or higher',
 				'complianz-gdpr' ) );
 		}
 
 		global $wp_version;
 		if ( version_compare( $wp_version, '4.9', '<' ) ) {
 			deactivate_plugins( plugin_basename( __FILE__ ) );
-			wp_die( __( 'Complianz GDPR cannot be activated. The plugin requires WordPress 4.6 or higher',
+			wp_die( __( 'Complianz GDPR cannot be activated. The plugin requires WordPress 4.9 or higher',
 				'complianz-gdpr' ) );
 		}
 	}
