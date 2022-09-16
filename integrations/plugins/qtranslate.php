d034d999d8aeeb6655179fee59b1a1aece15b2d4
sviluppomania <63558798+sviluppomania@users.noreply.github.com>
2022-09-17 00:17:05 +0200

    Update qtranslate.php

--- integrations/plugins/qtranslate.php
+++ integrations/plugins/qtranslate.php
@@ -9,8 +9,10 @@ function cmplz_qtranslatex_options($options){
 	$keys = array(
 		'header',
 		'categories',
+        'accept',
 		'accept_optin',
 		'accept_optout',
+		'accept_all',
 		'manage_consent',
 		'manage_options',
 		'save_settings',
@@ -19,6 +21,8 @@ function cmplz_qtranslatex_options($options){
 		'message_optin',
 		'category_functional',
 		'category_preferences',
+		'save_preferences',
+		'view_preferences',
 		'category_statistics',
 		'functional_text',
 		'statistics_text',
@@ -26,12 +30,15 @@ function cmplz_qtranslatex_options($options){
 		'preferences_text',
 		'marketing_text',
 		'category_marketing',
+		'revoke',
+        'readmore_optin',
+        'categories',
 	);
 
 	foreach($keys as $key){
-		if( isset($options[$key]) ){
-			$options[$key] = __($options[$key]);
-		}
+		if( isset($options[$key]) && is_string( $options[$key]) ){
+	        $options[$key] = __($options[$key]);
+        }
 	}
 	return $options;
 }
