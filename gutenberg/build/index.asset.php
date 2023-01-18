3273f0c4a216eb049232541e541dc13853930b1e
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-01-18 08:41:07 +0100

    build

--- gutenberg/build/index.asset.php
+++ gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '412c433bc00fecc48e51');
+<?php return array('dependencies' => array('wp-element'), 'version' => '9f777bf5322a7d043661');
