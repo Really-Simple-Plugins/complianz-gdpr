3af719eee6496f64e00bd529087271086c7bd729
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-02-07 20:10:45 +0100

    ABSPATH

--- DNSMPD/csv.php
+++ DNSMPD/csv.php
@@ -10,8 +10,8 @@ if ( !file_exists(BASE_PATH . 'wp-load.php') ) {
 	die("WordPress not installed here");
 }
 require_once( BASE_PATH . 'wp-load.php' );
-require_once( BASE_PATH . 'wp-includes/class-phpass.php' );
-require_once( BASE_PATH . 'wp-admin/includes/image.php' );
+require_once( ABSPATH . 'wp-includes/class-phpass.php' );
+require_once( ABSPATH . 'wp-admin/includes/image.php' );
 
 if ( isset( $_GET['nonce'] ) ) {
 	$nonce = $_GET['nonce'];
