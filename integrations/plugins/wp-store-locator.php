3b1a0c331de2dd7afef208b721a6cc5c248ab4f0
jarno-vos <78414123+jarno-vos@users.noreply.github.com>
2023-05-02 11:29:41 +0200

    add to detected services

--- integrations/plugins/wp-store-locator.php
+++ integrations/plugins/wp-store-locator.php
@@ -30,3 +30,18 @@ function cmplz_wpsl_googlemaps_script( $tags ) {
 	return $tags;
 }
 add_filter( 'cmplz_known_script_tags', 'cmplz_wpsl_googlemaps_script' );
+
+/**
+ * Add services to the list of detected items, so it will get set as default, and will be added to the notice about it
+ *
+ * @param $services
+ *
+ * @return array
+ */
+function cmplz_wpsl_googlemaps_detected_services( $services ) {
+	if ( ! in_array( 'google-maps', $services ) ) {
+		$services[] = 'google-maps';
+	}
+	return $services;
+}
+add_filter( 'cmplz_detected_services', 'cmplz_wpsl_googlemaps_detected_services' );
