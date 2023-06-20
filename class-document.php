20c0ef95968b557d5e25d1d57704c2bd1dcd4b4d
Mark <mark@really-simple-ssl.com>
2023-06-20 10:35:39 +0200

    antispambot

--- class-document.php
+++ class-document.php
@@ -1454,23 +1454,8 @@ if ( ! class_exists( "cmplz_document" ) ) {
 		 */
 
 		public function obfuscate_email( $email ) {
-			$alwaysEncode = array( '.', ':', '@' );
-			$result = '';
-			$email = strrev($email);
-			// Encode string using oct and hex character codes
-			for ( $i = 0; $i < strlen( $email ); $i ++ ) {
-				// Encode 25% of characters including several that always should be encoded
-				if ( in_array( $email[ $i ], $alwaysEncode ) || mt_rand( 1, 100 ) < 25 ) {
-					if ( mt_rand( 0, 1 ) ) {
-						$result .= '&#' . ord( $email[ $i ] ) . ';';
-					} else {
-						$result .= '&#x' . dechex( ord( $email[ $i ] ) ) . ';';
-					}
-				} else {
-					$result .= $email[ $i ];
-				}
-			}
-			return '<span class="cmplz-obfuscate" >'.$result.'</span>';
+            $result = antispambot( $email );
+            return '<span class="cmplz-obfuscate" >'.$result.'</span>';
 		}
 
 		/**
