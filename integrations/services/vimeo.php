1010fe48dfb8fa1079a21bc55d9df98bf27fcef3
Mark <mark@really-simple-ssl.com>
2023-07-28 08:36:42 +0200

    check thumbnail

--- integrations/services/vimeo.php
+++ integrations/services/vimeo.php
@@ -31,7 +31,7 @@ function cmplz_vimeo_placeholder( $placeholder_src, $src ) {
 		$new_src  = get_transient( "cmplz_vimeo_image_$vimeo_id" );
 		if ( ! $new_src || ! cmplz_file_exists_on_url( $new_src ) ) {
 			$data = json_decode( file_get_contents( 'http://vimeo.com/api/oembed.json?url=' . $src ) );
-			if (!empty($data) ) {
+			if ( ! empty( $data->thumbnail_url ) ) {
 				$placeholder_src = $data->thumbnail_url;
 				$placeholder_src = cmplz_download_to_site( $placeholder_src, 'vimeo' . $vimeo_id );
 				set_transient( "cmplz_vimeo_image_$vimeo_id", $placeholder_src, WEEK_IN_SECONDS );
