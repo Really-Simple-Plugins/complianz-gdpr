ad7dd0810a29d976a1cef3148c571aa21199ce13
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-10-19 09:55:39 +0200

    wp version

--- complianz-gpdr.php
+++ complianz-gpdr.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.wordpress.org/plugins/complianz-gdpr
  * Description: Complianz Privacy Suite for GDPR, CaCPA, DSVGO, AVG with a conditional cookie warning and customized cookie policy
  * Version: 7.0.0
- * Requires at least: 5.8
+ * Requires at least: 5.9
  * Requires PHP: 7.2
  * Text Domain: complianz-gdpr
  * Domain Path: /languages
