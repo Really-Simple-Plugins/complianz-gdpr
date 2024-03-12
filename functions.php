8557e571ce0224509a7bfd80390992cfd6fbe0b2
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-03-12 15:42:20 +0100

    update get

--- functions.php
+++ functions.php
@@ -1253,7 +1253,7 @@ if ( ! function_exists( 'cmplz_is_pagebuilder_preview' ) ) {
 			 || isset( $_GET['tatsu'] ) //tatsu
 			 || isset( $_GET['tatsu-header'] ) //tatsu
 			 || isset( $_GET['tatsu-footer'] ) //tatsu
-			 || isset( $_GET['ms_get_preview'] ) //meta slider
+			 || ( isset( $_GET['action'] ) && $_GET['action'] === 'ms_get_preview' ) //meta slider
 			 || strpos( $_SERVER['REQUEST_URI'], 'cornerstone/edit') !== false
 		) {
 			$preview = true;
